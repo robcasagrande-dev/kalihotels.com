1d463dcce696d9f33cd2f8d7ead74d843eef1666
Rob Casagrande <rob.casagrande.col@gmail.com>
2026-06-21 13:41:31 +0200

    feat: add German language translation

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -2,10 +2,11 @@ import es from '../i18n/es.json';
 import en from '../i18n/en.json';
 import it from '../i18n/it.json';
 import fr from '../i18n/fr.json';
+import de from '../i18n/de.json';
 
-const translations = { es, en, it, fr };
+const translations = { es, en, it, fr, de };
 
-export const LANGUAGES = ['es', 'en', 'it', 'fr'];
+export const LANGUAGES = ['es', 'en', 'it', 'fr', 'de'];
 export const DEFAULT_LANGUAGE = 'es';
 
 export function getTranslations(lang) {
